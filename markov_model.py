8432479533e3286807f0a9e51c7ad17d30b0e848
Ericius <ericius@linux-5k8y.suse>
2017-08-16 14:20:52 +0300

    Structure of class markov_model
    def _init_ is done

--- markov_model.py
+++ markov_model.py
@@ -1,11 +1,21 @@
 import random
 
 class markov_model:
-	
+	def _init_(self):
+		self.object = []
+		self.last_object = object()
+		self.last_generated_object = object()
+	def add(self, obj):
+		
+	def generate_next_object(self):
+		
+	def set_start_generate(self, start_with):
+		
 class object:
 	def _init_(self, data):
 		self.data = data
 		self.next = dict()
 		
 class markov_model_api:
+	
 	
