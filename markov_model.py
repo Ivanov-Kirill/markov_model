b2bfc690457089266a97de2cbf224e42c331fa69
Ericius <ericius@linux-5k8y.suse>
2017-08-16 14:43:32 +0300

    work with class markov_model
    remark - self.objects
    methods is done

--- markov_model.py
+++ markov_model.py
@@ -2,14 +2,39 @@ import random
 
 class markov_model:
 	def _init_(self):
-		self.object = []
+		self.objects = []
 		self.last_object = object()
 		self.last_generated_object = object()
 	def add(self, obj):
+		if(obj in self.objects):
+			count = self.last_object.next.get(obj)
+			if(count == None):
+				upd = {obj: 1}
+			else:
+				upd = {obj: count +1}
+			self.last_object.next.update(upd)
+		elif(len(self.objects != 0)):
+			upd = {obj: 1}
+			self.last_object.next.update(upd)
+			self.objects.append(obj)
+		else:
+			self.objects.append(obj)
+		self.last_object = obj
 		
 	def generate_next_object(self):
+		sum = 0
+		for _, count in  self.last_generated_object.next.items():
+			sum+=count
+		r = random.randint(1,sum)
+		for obj, count in self.last_generated_object.next.items():
+			r-=count
+			if(r<=0):
+				self.last_generated_object = obj
+				return self.last_generated_object
+		return None
 		
 	def set_start_generate(self, start_with):
+		self.last_generated_object = start_with
 		
 class object:
 	def _init_(self, data):
